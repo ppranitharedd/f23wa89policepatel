85bfaa0ff2050adfd70c46e8e9e6aaa643f0e6d0
ppranitharedd <policepatelpranitha03@gmail.com>
2023-10-25 17:22:13 -0500

    update computation

--- routes/computation.js
+++ routes/computation.js
@@ -4,19 +4,24 @@ var router = express.Router();
 
  
 
-var rand1 = Math.random() * 100;
+var rand1 = Math.random() * 200;
 var rand2 = Math.random() * 200;
 var num1 = 0;
 var num2 = 0;
 var num3 = 0;
+var num4 = 0;
 router.get('/', function (req, res, next) {
-  num1 = Math.sin(rand1);
-  num2 = Math.sin(rand2);
-  num3 = Math.sin(rand1);
+  num1 = Math.abs(rand1,rand2);
+  num2 = Math.acos(rand1);
+  num3 = Math.sinh(rand1);
+  num4 = Math.sin(rand1);
+
+
   res.send(`The Random numbers are ${rand1} and ${rand2}
-    <br> <br> sine applied to ${rand1} is ${num1}
-    <br> <br> sine applied to ${rand2} is ${num2}
-    <br> <br> sine applied to ${rand1} is ${num3}`);
+    <br> <br> abs applied to ${rand1} and ${rand2} is ${num1}
+    <br> <br> acos applied to ${rand1} is ${num2}
+    <br> <br> sinh applied to ${rand1} is ${num3}
+    <br> <br> sin applied to ${rand1} is ${num4}`);
 });
 
  
