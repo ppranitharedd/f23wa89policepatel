82f68cc9ab5fdad67dbcd35c348dfe1be86e746f
ppranitharedd <policepatelpranitha03@gmail.com>
2023-10-25 19:31:20 -0500

    update

--- routes/computation.js
+++ routes/computation.js
@@ -10,16 +10,17 @@ var num1 = 0;
 var num2 = 0;
 var num3 = 0;
 var num4 = 0;
+
+
 router.get('/', function (req, res, next) {
   num1 = Math.abs(rand1,rand2);
   num2 = Math.cos(rand1);
   num3 = Math.sinh(rand1);
   num4 = Math.sin(rand1);
 
-
   res.send(`The Random numbers are ${rand1} and ${rand2}
     <br> <br> abs applied to ${rand1} and ${rand2} is ${num1}
-    <br> <br> acos applied to ${rand1} is ${num2}
+    <br> <br> cos applied to ${rand1} is ${num2}
     <br> <br> sinh applied to ${rand1} is ${num3}
     <br> <br> sin applied to ${rand1} is ${num4}`);
 });
